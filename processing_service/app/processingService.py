6ea8bff559ddb3aee4820c85de18da77279c4764
unknown <taktuk07@gmail.com>
2026-03-05 10:59:01 +0200

    finish build the Transcription_service

--- processing_service/app/processingService.py
+++ processing_service/app/processingService.py
@@ -2,29 +2,11 @@ from uuid import uuid4
 from mongodb_connection import MongoDbService
 from elastic_service import ElasticService
 from logger import Logger
-import speech_recognition as sr
 logger = Logger.get_logger() 
 mongo = MongoDbService()
 elastic = ElasticService()
 
 class ProcessingService:
-    def add_id_for_data(self, data: dict):
-        data['audio_id'] = str(uuid4())
-        logger.info(f'id add to metadata')
-        return data
-    
-    def add_audio_to_str_to_metadata(self, data: dict):
-      try:
-        r = sr.Recognizer()
-        print(data)
-        with sr.AudioFile(data['matadata']['path']) as source:
-            audio = r.record(source)
-        data['string'] = r.recognize_sphinx(audio).lower()   
-        logger.info(f'convert audio to str') 
-        return data 
-      except sr.UnknownValueError as e:
-          logger.error(str(e))
-          raise str(e)
 
     def send_matadata_to_elastic(self, data: dict):
         try:
--- processing_service/app/send_service.py
+++ processing_service/app/send_service.py
@@ -5,7 +5,5 @@ logger = Logger.get_logger()
 
 class SendService:
     def send_to_mongo_and_elastic(self, data: dict):
-        processin.add_id_for_data(data=data)
-        processin.add_audio_to_str_to_metadata(data)
         processin.send_bysr_to_mangodb(data=data)
         processin.send_matadata_to_elastic(data=data)
